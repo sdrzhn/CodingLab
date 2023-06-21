80fffca7e5f3faaea296a7b3b068557c287af793
sdrzhn <sdrzhn_03@mail.ru>
2023-06-21 21:26:37 +0600

    final version

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "login_page";
+$conn = mysqli_connect($server, $user, $pass, $database);
+if (!$conn) {
+    die("<script>alert('Connection Failed.')</script>");
+}
+?>
